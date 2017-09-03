f0270f83429f28259f798912ef8f73f06e67cee6
kay8 <kay.yamagishi.work@gmail.com>
2017-09-02 19:03:46 -0700

    use class syntax to script

--- scripts/memorial.js
+++ scripts/memorial.js
@@ -7,11 +7,21 @@
 
 const moment = require('moment-timezone');
 
-module.exports = (robot => {
-  const starting_day_van = moment.tz('2017-07-30 23:31', 'America/Vancouver');
-  // const starting_day_ja = moment.tz(starting_day_van, 'Japan');
-  const today = moment().tz('America/Vancouver');
-  const days = today.diff(starting_day_van, 'days');
+class Memorial {
+  start(robot) {
+    const starting_day_van = moment.tz('2017-07-30 23:31', 'America/Vancouver');
+    // const starting_day_ja = moment.tz(starting_day_van, 'Japan');
+    const today = moment().tz('America/Vancouver');
+    const days = today.diff(starting_day_van, 'days');
 
-  return robot.hear(/記念日/i, res => res.send(`🎉 付き合い始めてから${days}日目😎`));
+    return robot.hear(/記念日/i, res => res.send(`🎉 付き合い始めてから${days}日目😎`));
+  }
+}
+
+module.exports = (robot => {
+
+  const memorial = new Memorial();
+  return memorial.start(robot);
 })
+
+module.exports.Memorial = Memorial;
