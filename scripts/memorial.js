c91ea2c9477239b71ca07a045257bf2a2cf933fa
kay8 <kay.yamagishi.work@gmail.com>
2017-09-04 01:26:15 -0700

    add cron job to notify message

--- scripts/memorial.js
+++ scripts/memorial.js
@@ -5,23 +5,65 @@
 
 'use strict';
 
-const moment = require('moment-timezone');
+const moment = require('moment-timezone')
+const memorial = {
+  fullDate: '2017-07-30 23:31',
+  noYearDate: '07-30 23:31',
+  tz: 'America/Vancouver',
+}
 
 class Memorial {
-  start(robot) {
-    const starting_day_van = moment.tz('2017-07-30 23:31', 'America/Vancouver');
-    // const starting_day_ja = moment.tz(starting_day_van, 'Japan');
-    const today = moment().tz('America/Vancouver');
-    const days = today.diff(starting_day_van, 'days');
 
-    return robot.hear(/記念日/i, res => res.send(`🎉 付き合い始めてから${days}日目😎`));
+  constructor(options) {
+    this.startingDayVan = moment.tz(memorial.fullDate, memorial.tz)
+    this.today = moment().tz(memorial.tz)
+  }
+
+  start(robot) {
+    const days = this.today.diff(this.startingDayVan, 'days')
+    return robot.hear(/記念日/i, res => res.send(`🎉 付き合い始めてから${days}日目`))
+  }
+
+  getNextMemorial() {
+    const currentYear = moment().tz(memorial.tz).year()
+    const nextYear = moment().add(1, 'year').tz(memorial.tz).year()
+
+    const currentYearDateStr = `${currentYear}-${memorial.noYearDate}`
+    const nextYearDateStr = `${nextYear}-${memorial.noYearDate}`
+
+    const currentYearDate = moment.tz(currentYearDateStr, memorial.tz)
+    const nextYearDate = moment.tz(nextYearDateStr, memorial.tz)
+
+    const diff = currentYearDate.diff(this.today, 'days')
+    let anniv;
+    if (diff === 0) {
+      anniv = currentYearDate.diff(this.startingDayVan, 'year')
+      return `🎉${anniv}周年です`
+    } else if (diff < 0) {
+      anniv = nextYearDate.diff(this.today, 'days')
+      return `次の記念日まで${anniv}日です`
+    } else {
+      anniv = diff
+      return `次の記念日まで${anniv}日です`
+    }
+  }
+
+  get_morning_message(callback) {
+    return callback(this.getNextMemorial())
+  }
+
+  // Instant interface
+  static get_morning_message(callback){
+    const memorial = new Memorial();
+    return memorial.get_morning_message(callback);
   }
 }
 
 module.exports = (robot => {
 
-  const memorial = new Memorial();
-  return memorial.start(robot);
+  const memorial = new Memorial()
+  return memorial.start(robot)
+
 })
 
-module.exports.Memorial = Memorial;
+module.exports.Memorial = Memorial
--- scripts/morning.js
+++ scripts/morning.js
@@ -17,16 +17,16 @@ const options = {
 
 class Morning {
   constructor(options) {
-    this.channel = options.channel
-    this.timezone = options.timeZone
-    this.cronTime = options.cronTime
+    this.channel = 'general'
+    this.timezone = 'America/Vancouver'
+    this.cronTime = '0 0 7 * * *'
   }
 
   start(robot) {
-    const job = new CronJob({
+    const job = new cronJob({
       cronTime: this.cronTime,
       onTick: () => {
-        return this.greet(robot)
+        return this.notify(robot)
       },
       start: true,
       timeZone: this.timeZone,
@@ -35,8 +35,23 @@ class Morning {
     return job
   }
 
-  test(robot) {
-    //robot.messageRoom this.channel, 'test'
+  notify(robot) {
+    this.getMemorial(message => {
+      return robot.messageRoom(this.channel, message);
+    });
+  }
+
+  getMemorial(callback) {
+    return memorial.get_morning_message(callback);
   }
 
 }
+
+module.exports = (robot => {
+
+  const morning = new Morning()
+  return morning.start(robot)
+
+})
+
+module.exports.Morning = Morning
