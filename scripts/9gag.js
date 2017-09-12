dfc83837dfa66c3913f2e0666b5a9a7624cc1c2a
kay8 <kay.yamagishi.work@gmail.com>
2017-09-12 15:48:54 -0700

    convert es6 and fix

--- scripts/9gag.js
+++ scripts/9gag.js
@@ -19,41 +19,40 @@
 
 const Select      = require( "soupselect" ).select
 const HTMLParser  = require("htmlparser")
+const request = require('request')
+
+const url = "https://9gag.com/random"
 
 class Ninegag {
 
   start(robot) {
-    return robot.respond(/9gag/i, message =>
-      this.send_meme(message, (title, src) => message.send(title, src))
-    )
+    return robot.respond(/9gag/i, res => this.request_meme(res))
   }
 
-  send_meme(message, response_handler) {
-    const url = "https://9gag.com/random"
+  request_meme(res) {
+    return request.get(url, (error, response, body) => {
 
-    return message.http( url ).get()(function(error, response, body) {
-      if (error) {
-        return response_handler("Sorry, something went wrong")
+      if (!error && (response.statusCode === 200)) {
+        console.log("Request success.")
+
+        const selectors = ["a img.badge-item-img"]
+        if ((process.env.HUBOT_9GAG_NO_GIFS == null)) {
+          selectors.unshift("div.badge-animated-container-animated img")
+        }
+
+        let img_src = this.get_meme_image(body, selectors)
+        if (img_src.substr(0, 5) !== "https") {
+          img_src = `https:${img_src}`
+        }
+
+        const img_title = this.escape_html_characters( this.get_meme_title( body, [".badge-item-title"] ) )
+
+        return res.send(img_title, img_src)
+
+      } else {
+        console.log("Request error.")
+        return res.send('Request error.')
       }
-
-      // if (response.statusCode === 302) {
-      //   location = response.headers['location']
-      //   return send_meme( message, location, response_handler )
-      // }
-
-      const selectors = ["a img.badge-item-img"]
-      if ((process.env.HUBOT_9GAG_NO_GIFS == null)) {
-        selectors.unshift("div.badge-animated-container-animated img")
-      }
-
-      let img_src = this.get_meme_image( body, selectors );
-      if (img_src.substr(0, 5) !== "https") {
-        img_src = `https:${img_src}`
-      }
-
-      const img_title = escape_html_characters( this.get_meme_title( body, [".badge-item-title"] ) )
-
-      return response_handler(img_title, img_src)
     })
   }
 
@@ -63,7 +62,7 @@ class Ninegag {
 
     html_parser.parseComplete(body);
     for (let selector of Array.from(selectors)) {
-      const img_container = Select( html_handler.dom, selector )
+      const img_container = Select(html_handler.dom, selector)
       if (img_container && img_container[0]) {
         return img_container[0]
       }
